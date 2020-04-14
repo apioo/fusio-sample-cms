e9ab96574b7eed0db7c8b8ea5238eeb07b6f33c3
Christoph Kappestein <christoph.kappestein@gmail.com>
2020-04-14 19:54:48 +0200

    update php doc

--- src/Dependency/Container.php
+++ src/Dependency/Container.php
@@ -6,6 +6,12 @@ use App\Service\Page;
 use App\Service\Post;
 use Fusio\Impl\Dependency\Container as FusioContainer;
 
+/**
+ * Custom dependency container. We can create a new service by simply defining
+ * a new method which returns the service. Those services can be injected at
+ * constructor argument at any action. Therefor you only need to specify the
+ * fitting type-hint and the DIC injects the fitting service
+ */
 class Container extends FusioContainer
 {
     public function getPageService(): Page
