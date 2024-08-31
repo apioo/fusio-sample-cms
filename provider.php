bc3ee2a12c850a8e9222c20f66962cb6b57d5916
Christoph Kappestein <christoph.kappestein@gmail.com>
2024-08-31 23:15:09 +0200

    update config

--- provider.php
+++ provider.php
@@ -1,14 +1,5 @@
 <?php
 
 return [
-    \Fusio\Adapter\Cli\Adapter::class,
-    \Fusio\Adapter\Fcgi\Adapter::class,
-    \Fusio\Adapter\File\Adapter::class,
-    \Fusio\Adapter\GraphQL\Adapter::class,
-    \Fusio\Adapter\Http\Adapter::class,
-    \Fusio\Adapter\Php\Adapter::class,
-    \Fusio\Adapter\Smtp\Adapter::class,
-    \Fusio\Adapter\Soap\Adapter::class,
-    \Fusio\Adapter\Sql\Adapter::class,
-    \Fusio\Adapter\Util\Adapter::class,
+    \Fusio\Adapter\Stripe\Adapter::class,
 ];
